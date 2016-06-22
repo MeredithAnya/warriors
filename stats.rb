66d21324a563e876d70f1beefe3bf219b5eccabe
Meredith Heller <meredith.a.heller@gmail.com>
2016-06-22 14:52:34 -0700

    change format

--- stats.rb
+++ stats.rb
@@ -6,8 +6,6 @@ class NBAPlayer
   end
 
   def add_to_average(attribute, data)
-    puts attribute
-    puts data
     current_average = self.send(attribute.to_sym)
     if current_average.nil?
       self.send("#{attribute}=".to_sym, data)
@@ -25,38 +23,8 @@ class NBAPlayer
     end
   end
 
-
 end
-curry =  NBAPlayer.new("Stephen", "Curry")
-green =  NBAPlayer.new("Draymond", "Green")
-ezeli =  NBAPlayer.new("Festus", "Ezeli")
-thompson = NBAPlayer.new("Klay", "Thompson")
-barnes = NBAPlayer.new("Harrison", "Barnes")
-bogut = NBAPlayer.new("Andrew", "Bogut")
-iguodala = NBAPlayer.new("Andre", "Iguodala")
-livingston = NBAPlayer.new("Shaun", "Livingston")
-clark = NBAPlayer.new("Ian", "Clark")
-mcadoo = NBAPlayer.new("James Michael McAdoo")
-rush = NBAPlayer.new("Brandon", "Rush")
-speights = NBAPlayer.new("Marreese", "Speights")
-barbosa = NBAPlayer.new("Leandro", "Barbosa")
-jthompson = NBAPlayer.new("Jason", "Thompson")
 
-
-PLAYERS = {"Stephen Curry" => curry,
-            "Draymond Green" => green,
-            "Festus Ezeli" => ezeli,
-            "Klay Thompson" => thompson,
-            "Harrison Barnes" => barnes,
-            "Andrew Bogut" => bogut,
-            "Andre Iguodala" => iguodala,
-            "Shaun Livingston" => livingston,
-            "Ian Clark" => clark,
-            "James Michael McAdoo" => mcadoo,
-            "Brandon Rush" => rush,
-            "Marreese Speights" => speights
-            "Jason Thompson" => jthompson,
-            "Leandro Barbosa" => barbosa}
 
 STATS_HASH = {
   1 => "MINS",
@@ -80,16 +48,16 @@ STATS_HASH = {
   19 => "PTS"
 }
 
-def find_player(firstname, lastname)
+def find_player(firstname, lastname, players)
   fullname = firstname + " " + lastname
   player = nil
-  PLAYERS.each do |name, object|
-    player = object if name = fullname
+  players.each do |name, object|
+    player = object if name == fullname
   end
   player
 end
 
-def handle_stats(stats)
+def handle_stats(stats, players)
   firstname = stats.first
   lastname = stats[1]
   count = 2
@@ -108,7 +76,7 @@ def handle_stats(stats)
     count = 4
   end
 
-  player = find_player(firstname, lastname)
+  player = find_player(firstname, lastname, players)
   stats_num = 1
 
   if player
@@ -119,20 +87,65 @@ def handle_stats(stats)
       data = stats[ind].to_f
       player.add_to_average(attribute, data)
       stats_num += 1
+    end
   end
   player
 end
 
 def readfile
-  final = []
-  (1...5).each do |num|
+  curry =  NBAPlayer.new("Stephen", "Curry")
+  green =  NBAPlayer.new("Draymond", "Green")
+  ezeli =  NBAPlayer.new("Festus", "Ezeli")
+  thompson = NBAPlayer.new("Klay", "Thompson")
+  barnes = NBAPlayer.new("Harrison", "Barnes")
+  bogut = NBAPlayer.new("Andrew", "Bogut")
+  iguodala = NBAPlayer.new("Andre", "Iguodala")
+  livingston = NBAPlayer.new("Shaun", "Livingston")
+  clark = NBAPlayer.new("Ian", "Clark")
+  mcadoo = NBAPlayer.new("James Michael", "McAdoo")
+  rush = NBAPlayer.new("Brandon", "Rush")
+  speights = NBAPlayer.new("Marreese", "Speights")
+  barbosa = NBAPlayer.new("Leandro", "Barbosa")
+  jthompson = NBAPlayer.new("Jason", "Thompson")
+
+  players = {"Stephen Curry" => curry,
+              "Draymond Green" => green,
+              "Festus Ezeli" => ezeli,
+              "Klay Thompson" => thompson,
+              "Harrison Barnes" => barnes,
+              "Andrew Bogut" => bogut,
+              "Andre Iguodala" => iguodala,
+              "Shaun Livingston" => livingston,
+              "Ian Clark" => clark,
+              "James Michael McAdoo" => mcadoo,
+              "Brandon Rush" => rush,
+              "Marreese Speights" => speights,
+              "Jason Thompson" => jthompson,
+              "Leandro Barbosa" => barbosa}
+
+
+  (1...11).each do |num|
     a = File.new("./game_stats/game#{num}.txt")
     words = []
     a.readlines.each do |line|
       stats = line.split(" ")
-      handle_stats(stats)
+      handle_stats(stats, players)
     end
-    final << words
   end
-  final
+  players
 end
+
+# STATS I CARE ABOUT:
+
+# [17] pry(main)> players["Stephen Curry"].TPP
+# => 45.71818181818182
+# [18] pry(main)> players["Stephen Curry"].FGP
+# => 52.809090909090905
+# [19] pry(main)> players["Stephen Curry"].FTP
+# => 90.87272727272729
+# [20] pry(main)> players["Stephen Curry"].REB
+# => 5.363636363636363
+# [21] pry(main)> players["Stephen Curry"].AST
+# => 5.7272727272727275
+# [22] pry(main)> players["Stephen Curry"].PTS
+# => 33.90909090909091
